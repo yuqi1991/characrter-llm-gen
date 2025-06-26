0e7ad3bfb1a15d2fb7f97541b927329bac9768ad
yuqu.li <yuqi.li.91@gmail.com>
2025-06-26 19:14:09 +0800

    update log

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
+    filename="logs/app.log",
 )
 logger = logging.getLogger(__name__)
 
